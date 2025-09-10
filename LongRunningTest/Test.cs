916c4dedc6cc2bc4d14d90edf9ca3867fd2ca776
unknown <gwayden@yandex.ru>
2025-09-10 14:43:19 +0500

    added test classes

---
+++ LongRunningTest/Test.cs
@@ -0,0 +1,10 @@
+﻿namespace LongRunningTest;
+
+public abstract class Test
+{
+    private bool testSuccessful;
+    private string error = "";
+    private int testDuration;
+    public abstract void Run();
+    public abstract Dictionary<string,string> ReturnResults();
+}
